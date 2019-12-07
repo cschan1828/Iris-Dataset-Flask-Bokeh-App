c8341a43ca49fc10d00281731b30df67d09d0a1f
CS Chan <cschan1828@gmail.com>
2019-12-07 21:18:45 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import math
+import numpy as np
+import pandas as pd
+
+from bokeh.embed import components
+from bokeh.layouts import column, gridplot, layout, row
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.plotting import figure
+
+from flask import Flask, render_template, request
+
+df = pd.read_csv('data/iris.csv')
+
+df_Setosa = df.loc[df.iris_class=='Setosa']
+df_Setosa['color'] = 'firebrick'
+df_Versicolor = df.loc[df.iris_class=='Versicolor']
+df_Versicolor['color'] = 'limegreen'
+df_Virginica = df.loc[df.iris_class=='Virginica']
+df_Virginica['color'] = 'deepskyblue'
+
+
+def sepal_dim(df):
+    def sepal_dim_plot(df_class):
+        source = ColumnDataSource(df_class)
+        hover = HoverTool(tooltips=[('sepal length','@sepal_length'), ('sepal width','@sepal_width')])
+        p = figure(
+            title=df_class.iris_class.unique().tolist().pop(),
+            x_axis_label='sepal length',
+            y_axis_label='sepal width',
+            plot_width=380,
+            plot_height=380,
+        )
+        p.circle(
+            x='sepal_length',
+            y='sepal_width',
+            color='color',
+            alpha=0.7,
+            hover_alpha=0,
+            source=source
+            )
+        p.add_tools(hover)
+
+        return p
+
+    p_Setosa = sepal_dim_plot(df_Setosa)
+    p_Versicolor = sepal_dim_plot(df_Versicolor)
+    p_Virginica = sepal_dim_plot(df_Virginica)
+
+    p_Setosa.x_range = p_Versicolor.x_range = p_Virginica.x_range
+    p_Setosa.y_range = p_Versicolor.y_range = p_Virginica.y_range
+
+    layout = row(p_Setosa, p_Versicolor, p_Virginica)
+
+    return layout
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    sepal_dim_chart = sepal_dim(df)
+    script, div = components(sepal_dim_chart)
+
+    return render_template('index.html', script=script,  div=div)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ readme.md
@@ -0,0 +1,23 @@
+# FLASK - IRIS dataset exploration app
+
+This is a Bokeh dashboard application with interactive charts for IRIS data set.
+
+## Requirements
+
+Required packages are listed in requirements.txt
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+export FLASK_APP=app.py
+```
+
+```bash
+flask run
+```
+
+## Data source
+UC Irvine Machine Learning Repository.
+https://archive.ics.uci.edu/ml/datasets/iris
